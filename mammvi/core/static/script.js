03c6ca1f9db47689fef74712c4e11a7ff5c06ace
V01Ddev <x.v.0.1.d.xhacking@gmail.com>
2024-06-19 19:10:09 +0300

    Final Working Jquery + mainly finished CSS + Need to working on splitting site into templates + Added statics for initial deployment + __pycache__ is still being tracked??

--- mammvi/core/static/script.js
+++ mammvi/core/static/script.js
@@ -1,10 +1,6 @@
 
-console.log("Loading page...")
-
 $(document).ready(function() {
-    console.log("Page loaded")
     $(".images-to-click img").click(function(){
-        console.log("Image clicked")
         $(".full-image").attr("src", $(this).attr("src"));
         $('.image-viewer').show();
 

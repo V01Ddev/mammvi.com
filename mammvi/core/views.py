13f8b5ccadbe2e7077a84c703b771558e8f55ffa
v01d.dev <x.v.0.1.d.xhacking@gmail.com>
2024-06-20 00:37:31 +0300

    Added content + Split content and base htmls + Renamed and created script to rename the images + Not adding scroll down sym for now

--- mammvi/core/views.py
+++ mammvi/core/views.py
@@ -3,5 +3,5 @@ from django.template import loader
 
 
 # Create your views here.
-def index(request):
-    return render(request, "index.html")
+def content(request):
+    return render(request, "content.html")

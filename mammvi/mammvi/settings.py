ff26ff7bc37a87a783f2d15ac6e9b368c2adb0c0
v01d.dev <x.v.0.1.d.xhacking@gmail.com>
2024-06-22 01:21:12 +0300

    Fixed importing key from a file

--- mammvi/mammvi/settings.py
+++ mammvi/mammvi/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import key
-SECRET_KEY = 'key.key'
+from .key import key
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

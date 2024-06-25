1b67903e825f54d60325e0a6c99838cdecd07bf7
v01d.dev <x.v.0.1.d.xhacking@gmail.com>
2024-06-25 18:03:27 +0300

    Removed content.html all in index.php

--- index.php
+++ index.php
@@ -23,15 +23,87 @@
         </section>
     </head>
 
-    <!--
-        [1] Section title using <h3>
-        [2] date taken, or range of dates
-        [3] use p tag to write paragraph about the photos
-    -->
 
     <body>
-<?php 
-include "content.html";
-?>
+
+        <section class="23_01_27">
+            <h3>Qatar Racing Club</h3>
+            <h5>27th January 2023</h5>
+            <p>
+            Photos from Qatar Racing Club
+            </p>
+            <div class="images-to-click">
+                <ul>
+                    <li>
+                        <img src='/images/23_01_27/1.JPG' class="the-images" loading="lazy">
+                    </li>
+
+                    <li>
+                        <img src='/images/23_01_27/2.JPG' class="the-images" loading="lazy">
+                    </li>
+
+                    <li>
+                        <img src='/images/23_01_27/3.JPG' class="the-images" loading="lazy">
+                    </li>
+
+                    <li>
+                        <img src='/images/23_01_27/5.JPG' class="the-images" loading="lazy">
+                    </li>
+
+                    <li>
+                        <img src='/images/23_01_27/6.JPG' class="the-images" loading="lazy">
+                    </li>
+
+                    <li>
+                        <img src='/images/23_01_27/7.JPG' class="the-images" loading="lazy">
+                    </li>
+
+                    <li>
+                        <img src='/images/23_01_27/8.JPG' class="the-images" loading="lazy">
+                    </li>
+
+                    <li>
+                        <img src='/images/23_01_27/9.JPG' class="the-images" loading="lazy">
+                    </li>
+
+                    <li>
+                        <img src='/images/23_01_27/10.JPG' class="the-images" loading="lazy">
+                    </li>
+
+                    <li>
+                        <img src='/images/23_01_27/11.JPG' class="the-images" loading="lazy">
+                    </li>
+                </ul>
+            </div>
+        </section>
+
+        <section class='23_06_29'></section>
+
+        <h3>Turkey 2023</h3>
+        <h5>29th June 2023</h5>
+        <p>
+        Random street photography in Istanbul
+        </p>
+        <div class="images-to-click">
+            <ul>
+                <li>
+                    <img src='/images/23_06_29/1.jpg' class="the-images" loading="lazy">
+                </li>
+
+                <li>
+                    <img src='/images/23_06_29/2.jpg' class="the-images" loading="lazy">
+                </li>
+
+                <li>
+                    <img src='/images/23_06_29/3.jpg' class="the-images" loading="lazy">
+                </li>
+            </ul>
+        </div>
+
+        <!-- Image full screen view -->
+        <div class="image-viewer">
+            <img class="full-image">
+        </div>
+
     </body>
 </html>

5d08d1ce463424c18f0ed7af329055bed47a769d
v01d.dev <x.v.0.1.d.xhacking@gmail.com>
2024-06-26 22:07:30 +0300

    Added image-div to maybe fix safari bug

--- index.php
+++ index.php
@@ -29,48 +29,68 @@
             <h3>Qatar Racing Club</h3>
             <h5>27th January 2023</h5>
             <p>
-            Photos from Qatar Racing Club
+                Photos from Qatar Racing Club
             </p>
             <div class="images-to-click">
                 <ul>
                     <li>
-                        <img src='/images/23_01_27/1.JPG' class="the-images" loading='lazy'>
+                        <div class="image-div">
+                            <img src='/images/23_01_27/1.JPG' class="the-images" loading='lazy'>
+                        </div>
                     </li>
 
                     <li>
-                        <img src='/images/23_01_27/2.JPG' class="the-images" loading='lazy'>
+                        <div class="image-div">
+                            <img src='/images/23_01_27/2.JPG' class="the-images" loading='lazy'>
+                        </div>
                     </li>
 
                     <li>
-                        <img src='/images/23_01_27/3.JPG' class="the-images" loading='lazy'>
+                        <div class="image-div">
+                            <img src='/images/23_01_27/3.JPG' class="the-images" loading='lazy'>
+                        </div>
                     </li>
 
                     <li>
-                        <img src='/images/23_01_27/5.JPG' class="the-images" loading='lazy'>
+                        <div class="image-div">
+                            <img src='/images/23_01_27/5.JPG' class="the-images" loading='lazy'>
+                        </div>
                     </li>
 
                     <li>
-                        <img src='/images/23_01_27/6.JPG' class="the-images" loading='lazy'>
+                        <div class="image-div">
+                            <img src='/images/23_01_27/6.JPG' class="the-images" loading='lazy'>
+                        </div>
                     </li>
 
                     <li>
-                        <img src='/images/23_01_27/7.JPG' class="the-images" loading='lazy'>
+                        <div class="image-div">
+                            <img src='/images/23_01_27/7.JPG' class="the-images" loading='lazy'>
+                        </div>
                     </li>
 
                     <li>
-                        <img src='/images/23_01_27/8.JPG' class="the-images" loading='lazy'>
+                        <div class="image-div">
+                            <img src='/images/23_01_27/8.JPG' class="the-images" loading='lazy'>
+                        </div>
                     </li>
 
                     <li>
-                        <img src='/images/23_01_27/9.JPG' class="the-images" loading='lazy'>
+                        <div class="image-div">
+                            <img src='/images/23_01_27/9.JPG' class="the-images" loading='lazy'>
+                        </div>
                     </li>
 
                     <li>
-                        <img src='/images/23_01_27/10.JPG' class="the-images" loading='lazy'>
+                        <div class="image-div">
+                            <img src='/images/23_01_27/10.JPG' class="the-images" loading='lazy'>
+                        </div>
                     </li>
 
                     <li>
-                        <img src='/images/23_01_27/11.JPG' class="the-images" loading='lazy'>
+                        <div class="image-div">
+                            <img src='/images/23_01_27/11.JPG' class="the-images" loading='lazy'>
+                        </div>
                     </li>
                 </ul>
             </div>
@@ -80,20 +100,26 @@
         <h3>Turkey 2023</h3>
         <h5>29th June 2023</h5>
         <p>
-        Random street photography in Istanbul
+            Random street photography in Istanbul
         </p>
         <div class="images-to-click">
             <ul>
                 <li>
-                    <img src='/images/23_06_29/1.jpg' class="the-images" loading='lazy'>
+                    <div class="image-div">
+                        <img src='/images/23_06_29/1.jpg' class="the-images" loading='lazy'>
+                    </div>
                 </li>
 
                 <li>
-                    <img src='/images/23_06_29/2.jpg' class="the-images" loading='lazy'>
+                    <div class="image-div">
+                        <img src='/images/23_06_29/2.jpg' class="the-images" loading='lazy'>
+                    </div>
                 </li>
 
                 <li>
-                    <img src='/images/23_06_29/3.jpg' class="the-images" loading='lazy'>
+                    <div class="image-div">
+                        <img src='/images/23_06_29/3.jpg' class="the-images" loading='lazy'>
+                    </div>
                 </li>
             </ul>
         </div>

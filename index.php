922147c64dcf4f51c11f86debaebaf0885120166
v01d.dev <x.v.0.1.d.xhacking@gmail.com>
2024-06-30 01:44:59 +0300

    Changed to used thm for faster loading + changed JS to switch to full sized image when in full screen

--- index.php
+++ index.php
@@ -35,61 +35,61 @@
                 <ul>
                     <li>
                         <div class="image-div">
-                            <img src='/images/23_01_27/1.JPG' class="the-images" loading='lazy'>
+                            <img src='/images/23_01_27/thm/1.JPG' class="the-images" loading='lazy'>
                         </div>
                     </li>
 
                     <li>
                         <div class="image-div">
-                            <img src='/images/23_01_27/2.JPG' class="the-images" loading='lazy'>
+                            <img src='/images/23_01_27/thm/2.JPG' class="the-images" loading='lazy'>
                         </div>
                     </li>
 
                     <li>
                         <div class="image-div">
-                            <img src='/images/23_01_27/3.JPG' class="the-images" loading='lazy'>
+                            <img src='/images/23_01_27/thm/3.JPG' class="the-images" loading='lazy'>
                         </div>
                     </li>
 
                     <li>
                         <div class="image-div">
-                            <img src='/images/23_01_27/5.JPG' class="the-images" loading='lazy'>
+                            <img src='/images/23_01_27/thm/5.JPG' class="the-images" loading='lazy'>
                         </div>
                     </li>
 
                     <li>
                         <div class="image-div">
-                            <img src='/images/23_01_27/6.JPG' class="the-images" loading='lazy'>
+                            <img src='/images/23_01_27/thm/6.JPG' class="the-images" loading='lazy'>
                         </div>
                     </li>
 
                     <li>
                         <div class="image-div">
-                            <img src='/images/23_01_27/7.JPG' class="the-images" loading='lazy'>
+                            <img src='/images/23_01_27/thm/7.JPG' class="the-images" loading='lazy'>
                         </div>
                     </li>
 
                     <li>
                         <div class="image-div">
-                            <img src='/images/23_01_27/8.JPG' class="the-images" loading='lazy'>
+                            <img src='/images/23_01_27/thm/8.JPG' class="the-images" loading='lazy'>
                         </div>
                     </li>
 
                     <li>
                         <div class="image-div">
-                            <img src='/images/23_01_27/9.JPG' class="the-images" loading='lazy'>
+                            <img src='/images/23_01_27/thm/9.JPG' class="the-images" loading='lazy'>
                         </div>
                     </li>
 
                     <li>
                         <div class="image-div">
-                            <img src='/images/23_01_27/10.JPG' class="the-images" loading='lazy'>
+                            <img src='/images/23_01_27/thm/10.JPG' class="the-images" loading='lazy'>
                         </div>
                     </li>
 
                     <li>
                         <div class="image-div">
-                            <img src='/images/23_01_27/11.JPG' class="the-images" loading='lazy'>
+                            <img src='/images/23_01_27/thm/11.JPG' class="the-images" loading='lazy'>
                         </div>
                     </li>
                 </ul>
@@ -106,19 +106,19 @@
             <ul>
                 <li>
                     <div class="image-div">
-                        <img src='/images/23_06_29/1.jpg' class="the-images" loading='lazy'>
+                        <img src='/images/23_06_29/thm/1.jpg' class="the-images" loading='lazy'>
                     </div>
                 </li>
 
                 <li>
                     <div class="image-div">
-                        <img src='/images/23_06_29/2.jpg' class="the-images" loading='lazy'>
+                        <img src='/images/23_06_29/thm/2.jpg' class="the-images" loading='lazy'>
                     </div>
                 </li>
 
                 <li>
                     <div class="image-div">
-                        <img src='/images/23_06_29/3.jpg' class="the-images" loading='lazy'>
+                        <img src='/images/23_06_29/thm/3.jpg' class="the-images" loading='lazy'>
                     </div>
                 </li>
             </ul>
--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,14 @@
 
 $(document).ready(function() {
+    // on image click, shows the full sized image full screen
     $(".images-to-click img").click(function(){
-        $(".full-image").attr("src", $(this).attr("src"));
+        var path = $(this).attr("src").replace('thm/', '')
+        console.log(path);
+        $(".full-image").attr("src", path);
         $('.image-viewer').show();
-
     });
 
+    // to hide the image if anywhere else other than the imaged is clicked 
     $(".image-viewer").on( "click", function() {
         $('.image-viewer').hide();
     });

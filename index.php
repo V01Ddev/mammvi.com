41a82d2e34763c73b00583c37e1ba235eae4dfdb
V01Ddev <x.v.0.1.d.xhacking@gmail.com>
2024-07-09 15:33:05 +0300

    Added images from Spain

--- index.php
+++ index.php
@@ -25,6 +25,64 @@
 
 
     <body>
+        <section class="24_06_27">
+            <h3>Spain</h3>
+            <h5>27th June 2024</h5>
+            <p>Pictures of the Alhambra palace in Granada</p>
+            <div class="images-to-click">
+                <ul>
+                    <li>
+                        <div class="image-div">
+                            <img src='/images/24_06_27/thm/1.jpg' class="the-images" loading='lazy'>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="image-div">
+                            <img src='/images/24_06_27/thm/2.jpg' class="the-images" loading='lazy'>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </section>
+
+        <section class="24_06_24">
+            <h5>24th June 2024</h5>
+            <p>Images taken in Córdoba of the Mosque-Cathedral</p>
+            <div class="images-to-click">
+                <ul>
+                    <li>
+                        <div class="image-div">
+                            <img src='/images/24_06_24/thm/1.jpg' class="the-images" loading='lazy'>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="image-div">
+                            <img src='/images/24_06_24/thm/2.jpg' class="the-images" loading='lazy'>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="image-div">
+                            <img src='/images/24_06_24/thm/3.jpg' class="the-images" loading='lazy'>
+                        </div>
+                    </li>
+            </div>
+        </section>
+
+        <section class="23_01_27">
+            <h5>20th June 2024</h5>
+            <div class="images-to-click">
+                <ul>
+                    <li>
+                        <div class="image-div">
+                            <img src='/images/24_06_20/thm/1.jpg' class="the-images" loading='lazy'>
+                        </div>
+                    </li>
+            </div>
+        </section>
+
         <section class="23_01_27">
             <h3>Qatar Racing Club</h3>
             <h5>27th January 2023</h5>
@@ -106,7 +164,7 @@
             <ul>
                 <li>
                     <div class="image-div">
-                        <img src='/images/23_06_29/thm/1.jpg' class="the-images" loading='lazy'>
+                        <img src='/images/23_06_29/1.jpg' class="the-images" loading='lazy'>
                     </div>
                 </li>
 
